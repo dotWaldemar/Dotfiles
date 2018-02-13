889740e6ecf4a45f6f51e2a805f82053f3442dc6
dotWaldemar <waldemar.dassler@gmail.com>
2018-02-13 15:04:37 +0200

    Include Oh-my-zsh installation

---
+++ zsh/setup.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env sh
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+touch ~/.hushlogin
+. ../scripts/functions.sh
+
+SOURCE="$(realpath .)"
+DESTINATION="$(realpath ~)"
+
+info "Setting up oh-my-zsh..."
+
+find . -name ".zshrc" | while read fn; do
+    fn=$(basename $fn)
+    symlink "$SOURCE/$fn" "$DESTINATION/$fn"
+done
+
+success "Finished setting up oh-my-zsh."
