b689acf1f0524027221cba7dba8a4ecba3bad23a
dotWaldemar <waldemar.dassler@gmail.com>
2018-02-28 10:14:26 +0200

    Added dnsmasq to brew install

--- scripts/awesome_header.sh
+++ scripts/awesome_header.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #
-#--Colors--------------------------------------
+# --Colors--------------------------------------
 #
 ESC_SEQ="\x1b["
 COL_RESET=$ESC_SEQ"39;49;00m"
@@ -22,10 +22,10 @@ function cool_header() {
     echo "\n"
 	echo "\n"
 	echo "\n"
-	echo "\n$COL_GREEN   ############################################## $COL_RESET"
-	echo "\n$COL_GREEN   #                 WALDEMAR                   # $COL_RESET"
-	echo "\n$COL_GREEN   #            Dotfiles for OSX                # $COL_RESET"
-	echo "\n$COL_GREEN   ############################################## $COL_RESET"
+	echo "\n$COL_GREEN      ############################################## $COL_RESET"
+	echo "\n$COL_GREEN      #                 WALDEMAR                   # $COL_RESET"
+	echo "\n$COL_GREEN      #            Dotfiles for OSX                # $COL_RESET"
+	echo "\n$COL_GREEN      ############################################## $COL_RESET"
 	echo "\n"
 	echo "\n"
 	echo "\n"
